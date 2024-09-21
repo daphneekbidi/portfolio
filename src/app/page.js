f579f28ab9206e37af9c13150e0bc1554b4359b3
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-09-21 16:08:31 +0700

    feat: Work section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/footer";
+import Work from "./components/work";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Work />
       <Footer />
     </>
   );
