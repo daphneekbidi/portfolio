4f589bb0376c2db5bcb2d28b71e7b41bcebcfdf2
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-09-25 09:22:28 +0700

    style: Multiple changes in work section

--- src/app/components/work.js
+++ src/app/components/work.js
@@ -1,39 +1,17 @@
 "use client";
-import React, { useEffect } from 'react';
 import Image from 'next/image';
 
 const Work = () => {
-    // useEffect(() => {
-    //     const textElement = document.getElementById('text-element');
-    //     const observer = new IntersectionObserver(
-    //       (entries) => {
-    //         entries.forEach((entry) => {
-    //           if (entry.isIntersecting) {
-    //             textElement.classList.add('opacity-50');
-    //           } else {
-    //             textElement.classList.remove('opacity-50');
-    //           }
-    //         });
-    //       },
-    //       { threshold: 0.5 } // Adjust the threshold as needed
-    //     );
-    
-    //     observer.observe(textElement);
-    
-    //     return () => {
-    //       observer.disconnect();
-    //     };
-    //   }, []);
     return (
         <section className="work-section">
-            <div className='relative flex justify-center bg-[#f1f2e0] py-16'>
-                <h2 id="text-element" className='top-40 space-x-10 absolute text-white font-bold text-8xl content-center opacity-100 mix-blend-difference'>
+            <div className='relative flex justify-center bg-gradient-to-b from-70% from-pistache py-16'>
+                <h2 id="text-element" className='top-44 space-x-10 absolute text-white font-bold text-8xl content-center opacity-100 mix-blend-difference md:text-[14rem]'>
                     <span className='inline-block'>W</span>
                     <span className='inline-block'>O</span>
                     <span className='inline-block'>R</span>
                     <span className='inline-block'>K</span>
                 </h2>
-                <Image className='max-h-60 max-w-60 rounded-2xl'
+                <Image className='max-h-60 max-w-60 rounded-2xl md:max-h-[28rem] md:max-w-[28rem]'
                     src="/work1.gif"
                     alt="Work display"
                     width={1920}
@@ -43,45 +21,91 @@ const Work = () => {
                     unoptimized
                 />
             </div>
-            <div className="work-content">
-                <p className="work-card-description">I maintained their websites from 2020 to 2022, EU and US. (Polaroid re-built their site recently) </p>
-                <div className='flex justify-center'>
-                    <div className="w-72 h-40">
-                        <a href="#">
-                            <Image 
-                                src="/polaroid-logo.png"
-                                alt="Polaroid Logo"
-                                width={200}
-                                height={200}
-                                layout="responsive"
-                            />
-                        </a>
+            <div className="p-8 md:w-[30rem] mx-auto">
+                <div className='space-y-4'>
+                    <p className="text-justify text-lg">
+                        I maintained their websites from 2020 to 2022, EU and US when I was part of the web agency AskPhill in the Netherlands. (Polaroid re-built their site recently) 
+                    </p>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40">
+                            <a href="#">
+                                <Image 
+                                    src="/polaroid-logo.png"
+                                    alt="Polaroid Logo"
+                                    width={200}
+                                    height={200}
+                                    layout="responsive"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40 content-center text-center">
+                            <a href="#" className='flex justify-center'>
+                                <Image 
+                                    src="/cluse.png"
+                                    alt="Cluse Logo"
+                                    width={200}
+                                    height={200}
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <p className="text-justify text-lg">
+                        I'm proud to have built parts of these websites working for the same web agency.
+                    </p>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40 content-center text-center">
+                            <a href="#" className='flex justify-center'>
+                                <Image 
+                                    src="/summum-logo.png"
+                                    alt="Summum Woman Logo"
+                                    width={200}
+                                    height={200}
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40 content-center text-center">
+                            <a href="#" className='flex justify-center'>
+                                <Image 
+                                    src="/north-action.png"
+                                    alt="North Action Group Logo"
+                                    width={200}
+                                    height={200}
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40 content-center text-center">
+                            <a href="#" className='flex justify-center'>
+                                <Image 
+                                    src="/pink-gellac.png"
+                                    alt="Pink Gellac Logo"
+                                    width={200}
+                                    height={200}
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <p className="text-justify text-lg">
+                        Here is a freelance project I did with LeylineXR for a National Geography event in 2024, where I did the UX design and front-end in React/Tailwind CSS.
+                    </p>
+                    <div className='flex justify-center border border-dashed border-black'>
+                        <div className="w-72 h-40 content-center text-center">
+                            <a href="#" className='flex justify-center'>
+                                <Image 
+                                    src="/leylineXr.png"
+                                    alt="Leyline XR project's Logo"
+                                    width={200}
+                                    height={200}
+                                />
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <div className='flex justify-center'>
-                    <div className="w-72 h-40 content-center text-center">
-                        <a href="#" className='flex justify-center'>
-                            <Image 
-                                src="/cluse-logo.webp"
-                                alt="Polaroid Logo"
-                                width={200}
-                                height={200}
-                            />
-                        </a>
-                    </div>
-                </div>
-                {/* <div className="work-card">
-                    <h3 className="work-card-title">Project 3</h3>
-                    <p className="work-card-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.</p>
-                    <a href="#" className="work-card-link">View Project
-                        <Image 
-                            src="/polaroid-logo.png"
-                            alt="Polaroid Logo"
-                            width={150}
-                            height={150}
-                        />
-                    </a>
-                </div> */}
             </div>
         </section>
     );
