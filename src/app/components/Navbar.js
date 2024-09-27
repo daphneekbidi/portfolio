4d04f61de1753b9fd3d5e6b143b1ab7a47f5abd1
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-09-27 15:32:00 +0700

    style: Add smooth scroll on click

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,21 +1,31 @@
+'use client'
 import React from 'react';
 
 const Navbar = () => {
+  const handleClick = (event) => {
+    event.preventDefault();
+    const element = document.getElementById(event.target.getAttribute('href').replace('#', ''));
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
       <>
-      <div className='flex justify-between items-center px-5 py-3'>
+      <div className='flex justify-between items-center px-5 py-3 sticky top-0 bg-white/75 z-20'>
         <a href="/" aria-label="Homepage" className='relative'>
           <div className='bg-pistache h-3 absolute top-3 left-[2px] w-[97%] -z-10 rounded-sm'></div>
           <span className='block text-2xl bold'>Daphnee KBIDI</span>
         </a>
         <nav aria-label="Main Navigation">
           <ul className="list-none flex gap-5">
-            <li className='text-lg no-underline focus:underline hover:underline'><a href="#work" aria-label="Work Section">Work</a></li>
+            <li className='text-lg no-underline focus:underline hover:underline'>
+              <a href="#work" aria-label="Work Section" onClick={handleClick}>Work</a>
+            </li>
             <li className='text-lg no-underline focus:underline hover:underline'><a href="#contact" aria-label="Contact Section">Contact</a></li>
           </ul>
         </nav>
       </div>
-      
       </>
   );
 };
