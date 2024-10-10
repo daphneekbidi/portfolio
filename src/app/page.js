369beb49251682c5e6a345626562262219b158b2
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-10-10 12:42:23 +0700

    feat: Skill section

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,15 @@
-import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import Footer from "./components/footer";
+import Header from "./components/Header";
+import Skills from './components/skills';
 import Work from "./components/work";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Skills />
       <Work />
       <Footer />
     </>
