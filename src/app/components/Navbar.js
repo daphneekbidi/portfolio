5def5e4cc083f70a17bc703c9cf428a5604d736a
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-10-21 21:03:48 +0700

    style: fix color

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -12,17 +12,17 @@ const Navbar = () => {
 
   return (
       <>
-      <div className='flex justify-between items-center px-5 py-3 sticky top-0 bg-white/75 z-20'>
+      <div className='flex justify-between items-center px-5 py-3 sticky top-0 bg-white z-20'>
         <a href="/" aria-label="Homepage" className='relative'>
           <div className='bg-pistache h-3 absolute top-3 left-[2px] w-[97%] -z-10 rounded-sm'></div>
-          <span className='block text-2xl bold'>Daphnee KBIDI</span>
+          <span className='block text-2xl bold text-black'>Daphnee KBIDI</span>
         </a>
         <nav aria-label="Main Navigation">
           <ul className="list-none flex gap-5">
-            <li className='text-lg no-underline focus:underline hover:underline'>
+            <li className='text-lg no-underline text-black focus:underline hover:underline'>
               <a href="#work" aria-label="Work Section" onClick={handleClick}>Work</a>
             </li>
-            <li className='text-lg no-underline focus:underline hover:underline'>
+            <li className='text-lg text-black no-underline focus:underline hover:underline'>
               <a href="mailto:daphneekbidi@gmail.com?subject=Contact%20Request&body=Hello," target="_blank" rel="noopener noreferrer" aria-label="Contact Section">Contact</a>
             </li>
           </ul>
