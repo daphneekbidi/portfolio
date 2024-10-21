1b2465a92a43b730f5c8980bfb5ca484afd6410d
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-10-21 21:20:29 +0700

    style: add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         'pistache': '#f1f2e0',
+        'blanc': '#fafafa',
       },
     },
   },
