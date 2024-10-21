db0e640fc77e0e1ec47a80e443ab8e680fb60112
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-10-21 21:02:26 +0700

    style: fix text color

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,11 +16,11 @@ const Header = () => {
           />
         </div>
         <div className="my-4 header-text md:w-1/2 content-center items-center">
-          <h1 className='text-5xl mt-8 mb-4'>
+          <h1 className='text-5xl mt-8 mb-4 text-black'>
             Hi I&apos;m a <strong>web developer</strong>, ecommerce enthousiast.
           </h1> 
-          <span className='block text-xl mt-2'>I come from Reunion Island, France and live now in Thailand.</span>
-          <p className='text-base text-justify mt-6'>
+          <span className='block text-xl mt-2 text-black'>I come from Reunion Island, France and live now in Thailand.</span>
+          <p className='text-base text-justify mt-6 text-black'>
             I love brewing tea in the morning and open my Macbook to code 💻 from home. When I&apos;m not squashing bugs, you&apos;ll find me daydreaming about the next project or petting my cats 🐱. I&apos;ve worked a lot with custom Shopify websites and React-based with Next.js web applications. Let&apos;s create something awesome together!
           </p>
         </div>
