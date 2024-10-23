03be7e0e08a9535226da0c611ba79fc44cddaef9
Daphnee KBIDI <daphneekbidi@gmail.com>
2024-10-23 10:42:50 +0700

    chore: ensure minification

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    swcMinify: true, // Ensure SWC minification is enabled
+  };
+  
+  export default nextConfig;
